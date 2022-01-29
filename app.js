14ffc405d6a33c73b1239dacdfeaaf09543b10d3
Giannis Fatouros <fatouros.ioannis@gmail.com>
2022-01-29 13:26:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+/* Dependencies */
 const express = require('express');
-const app = express();
+require('dotenv').config();
+/* Dependencies */
 
+/* GLOBAL variables */
 const PORT = 3000;
+/* GLOBAL variables */
+
+const app = express();
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
